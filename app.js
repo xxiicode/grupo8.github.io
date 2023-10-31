ee7c90e8c7bffa8ecf585e7e75d0528734b8f00e
xxiicode <leandrobilokapic@gmail.com>
2023-10-31 12:55:27 +0100

    iniciando

---
+++ app.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send("hola Express");
+});
+
+const PORT = 3000;
+
+app.listen(PORT, () => console.log(`http://localhost:${PORT}`));

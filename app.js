ab5a2ec797b1238ad5adb9ab9d7b95afa34e601e
xxiicode <leandrobilokapic@gmail.com>
2023-11-03 17:50:17 +0100

    test changes

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express();
+const pcolor = require("picocolors"); // para usar pcolor hay que declaralo asi: pcolor.red("hola")
 
 app.use(express.static("public"));
 
@@ -15,10 +16,22 @@ app.get('/nosotros', (req, res) => { // es para mostrar contenido fuera del publ
 res.sendFile(__dirname + '/nosotros.html')
 });
 
-app.get('/css', (req, res) => { // test para mostrar rutas dentro de public sin el '.html'
+app.get('/css', (req, res) => { // test para mostrar rutas dentro de public sin el ' .html'
     res.sendFile(__dirname + '/public/css/main.css')
     });
 
 const PORT = 3000;
-
 app.listen(PORT, () => console.log(`http://localhost:${PORT}`));
+
+
+// Path: 
+
+const path = require('node:path'); //llama el modulo nativo de node llamado path, que es para trabajar con rutas de archivos
+console.log(path.sep); //esto nos muestra el separador de rutas, en este caso es /
+
+console.log(pcolor.red('hola')); //escribe hola en la consola
+
+const test = require('./test'); // importa el archivo test.js, ose viene el chau! y el mensajeTest
+
+console.log(pcolor.blue(test)); // imprime en consola lo que esta en test.js y no tiene su propio console log.
+
---
+++ test.js
@@ -0,0 +1,6 @@
+const pcolor = require("picocolors") //si no lo declaro en el modulo no anda
+console.log(pcolor.green("chau!"));
+
+const mensajeTest = "esto es un mensaje de test";
+
+module.exports = mensajeTest;

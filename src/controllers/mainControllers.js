ca2119a0314dbe70ea968f2d93e92a8687c04642
Fernanda Isabel <fernanda.aranciaga@gmail.com>
2023-11-28 15:03:54 -0300

    se agrega mainLayout.ejs, cambios en mainControllers, home.ejs y wip.ejs

--- src/controllers/mainControllers.js
+++ src/controllers/mainControllers.js
@@ -1,9 +1,9 @@
 const mainControllers = {
-    home: (req, res) => res.render('home'),
+    home: (req, res) => res.render('home', {layout: "layouts/mainLayout"}),
     contact: (req, res) => res.send('Este es el contacto'),
     about: (req, res) => res.send('Este es el about'),
     faq: (req, res) => res.send('Este es el faq'),
-    wip: (req, res) => res.render("wip",{layout: "layouts/adminLayout"}), // esta usando el layout de admin, para test
+    wip: (req, res) => res.render("wip",{layout: "layouts/mainLayout"}), // esta usando el layout de admin, para test
 /*     e404: (req, res) => res.render('404') */
 }
 

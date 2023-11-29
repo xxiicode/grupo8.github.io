5df7b3f5384cc7aad6008ffa64ddba470d861fa8
María Agustina Sangiácomo <sangiacomoagus@gmail.com>
2023-11-29 11:14:25 -0300

    listo vista admin

--- src/controllers/adminController.js
+++ src/controllers/adminController.js
@@ -1,14 +1,16 @@
 const adminControllers = {
 //admin: (req, res) => res.send("Route for admin"),
 admin: (req, res) => res.render("admin", { layout: "layouts/adminLayout" }),
+//create: (req, res) => res.send("Route for admin create get"),
 create: (req, res) => res.render("create", { layout: "layouts/adminLayout"}),
+//create: (req, res) => res.send("Route for admin create post"),
 create: (req, res) => res.render("create", { layout: "layouts/adminLayout"}),
+//edit: (req, res) => res.send("Route for admin edit id get"),
 edit: (req, res) => res.render("edit", { layout: "layouts/adminLayout"}),
+//edit: (req, res) => res.send("Route for admin edit id put"),
 edit: (req, res) => res.render("edit", { layout: "layouts/adminLayout"}),
-//delete: (req, res) => res.render("delete", { layout: "layouts/adminLayout"}),
+delete: (req, res) => res.send("Route for admin delete id"),
 } 
-
-
 
 
     module.exports = adminControllers;
